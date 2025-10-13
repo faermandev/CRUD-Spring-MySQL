a188f33cf13e9e99dd01ae0db11bf60706743bad
David Faerman <faermandev>
2025-10-13 15:41:03 -0300

    first commit

---
+++ src/main/java/com/example/CRUD_Spring_mySQL/CrudSpringMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Spring_mySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUD_Spring_mySQL/CrudSpringMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Spring_mySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
