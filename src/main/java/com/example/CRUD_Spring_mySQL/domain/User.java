98cfc43037f3e005322f996f03b6b4dc053c3fae
David Faerman <faermandev>
2025-10-13 15:43:38 -0300

    User implementation

---
+++ src/main/java/com/example/CRUD_Spring_mySQL/domain/User.java
@@ -0,0 +1,42 @@
+package com.example.CRUD_Spring_mySQL.domain;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // banco gera o id
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+    
+}
